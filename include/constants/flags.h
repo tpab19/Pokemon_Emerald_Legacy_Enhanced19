c572bc14e3de4da0d3bf028c136ca042a952f637
Craig Livingstone <2470470l@student.gla.ac.uk>
2024-08-15 17:03:54 +0100

    Route 116 cut trees stay gone once cut

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1218,11 +1218,11 @@
 
 #define FLAG_HIDE_ROUTE_111_ROCK_1                                  0x493
 #define FLAG_HIDE_ROUTE_111_ROCK_2                                  0x494
-#define FLAG_UNUSED_0x495                                           0x495 // Unused Flag
-#define FLAG_UNUSED_0x496                                           0x496 // Unused Flag
-#define FLAG_UNUSED_0x497                                           0x497 // Unused Flag
-#define FLAG_UNUSED_0x498                                           0x498 // Unused Flag
-#define FLAG_UNUSED_0x499                                           0x499 // Unused Flag
+#define FLAG_HIDE_ROUTE_116_TREE_1                                  0x495
+#define FLAG_HIDE_ROUTE_116_TREE_2                                  0x496
+#define FLAG_HIDE_ROUTE_116_TREE_3                                  0x497
+#define FLAG_HIDE_ROUTE_116_TREE_4                                  0x498
+#define FLAG_HIDE_ROUTE_116_TREE_5                                  0x499
 #define FLAG_UNUSED_0x49A                                           0x49A // Unused Flag
 #define FLAG_UNUSED_0x49B                                           0x49B // Unused Flag
 #define FLAG_UNUSED_0x49C                                           0x49C // Unused Flag
