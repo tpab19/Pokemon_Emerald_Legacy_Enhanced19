f3535b6808ffd528d576f3741b2014306224f725
Craig Livingstone <2470470l@student.gla.ac.uk>
2024-09-24 18:47:15 +0100

    Fixed checkpartymon

--- src/field_specials.c
+++ src/field_specials.c
@@ -4301,3 +4301,27 @@ bool16 TryChangeDeoxysForm(void)
     gSpecialVar_Result = TRUE;
     return;
 }
+
+void CheckSpecies(void)
+{
+    u8 i;
+    u16 species;
+    struct Pokemon *pokemon;
+    for (i = 0; i < CalculatePlayerPartyCount(); i++)
+    {
+        pokemon = &gPlayerParty[i];
+        if (GetMonData(pokemon, MON_DATA_SANITY_HAS_SPECIES) && !GetMonData(pokemon, MON_DATA_IS_EGG))
+        {
+            species = GetMonData(pokemon, MON_DATA_SPECIES);
+            if (species == gSpecialVar_0x8005)
+            {
+                gSpecialVar_Result = TRUE;
+                return;
+            }
+            else
+            {
+                gSpecialVar_Result = FALSE;
+            }
+        }
+    }
+}
