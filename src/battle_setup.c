1a4bf2c1272b0a69eb1bfabd3a56cc4004275195
Craig Livingstone <2470470l@student.gla.ac.uk>
2024-11-16 22:43:15 +0000

    Legendary encounter music v2

--- src/battle_setup.c
+++ src/battle_setup.c
@@ -538,15 +538,22 @@ void BattleSetup_StartLegendaryBattle(void)
     case SPECIES_ENTEI:
     case SPECIES_RAIKOU:
     case SPECIES_SUICUNE:
-    case SPECIES_CELEBI:
         CreateBattleStartTask(B_TRANSITION_BLUR, MUS_RG_VS_LEGEND);
         break;
-    case SPECIES_MEW:
     case SPECIES_MEWTWO:
     case SPECIES_ZAPDOS:
     case SPECIES_MOLTRES:
     case SPECIES_ARTICUNO:
+        CreateBattleStartTask(B_TRANSITION_BLUR, MUS_VS_MEW);
+        break;
+    case SPECIES_MEW:
         CreateBattleStartTask(B_TRANSITION_GRID_SQUARES, MUS_VS_MEW);
+        break;
+    case SPECIES_CELEBI:
+        CreateBattleStartTask(B_TRANSITION_GRID_SQUARES, MUS_RG_VS_LEGEND);
+        break;
+    case SPECIES_JIRACHI:
+        CreateBattleStartTask(B_TRANSITION_GRID_SQUARES, MUS_RG_VS_DEOXYS);
         break;
     }
 
