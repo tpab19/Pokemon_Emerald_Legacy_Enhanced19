927e941bf9788e0d44fa2a25cc4260b03abecc53
Craig Livingstone <2470470l@student.gla.ac.uk>
2024-11-25 00:18:14 +0000

    Fixed hard mode rayquaza E4 block cheese

--- include/constants/vars.h
+++ include/constants/vars.h
@@ -112,7 +112,7 @@
 #define VAR_LILYCOVE_CITY_STATE                          0x405C // Unused Var
 #define VAR_MOSSDEEP_CITY_STATE                          0x405D
 #define VAR_SOOTOPOLIS_CITY_STATE                        0x405E
-#define VAR_EVER_GRANDE_CITY_STATE                       0x405F // Unused Var
+#define VAR_EVER_GRANDE_CITY_STATE                       0x405F
 #define VAR_ROUTE101_STATE                               0x4060
 #define VAR_ROUTE102_STATE                               0x4061 // Unused Var
 #define VAR_ROUTE103_STATE                               0x4062 // Unused Var
