007d262557c707c4fc7245e77b7cc4f4f514d8ca
Craig Livingstone <github@crzshadows.co.uk>
2024-12-29 20:21:06 +0000

    Added a post game incense mart in Slateport in case you sell them

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1271,7 +1271,7 @@
 #define FLAG_BEAT_ARCHIE_MARINE_CAVE                                0x4CB
 #define FLAG_BEAT_MAXIE_TERRA_CAVE                                  0x4CC
 #define FLAG_HIDE_ROUTE_103_TREE_1                                  0x4CD
-#define FLAG_UNUSED_0x4CE                                           0x4CE // Unused Flag
+#define FLAG_HIDE_SLATEPORT_CITY_INCENSE_WOMAN                      0x4CE
 #define FLAG_UNUSED_0x4CF                                           0x4CF // Unused Flag
 #define FLAG_UNUSED_0x4D0                                           0x4D0 // Unused Flag
 #define FLAG_UNUSED_0x4D1                                           0x4D1 // Unused Flag
