79396f881c16b4a4d57b63f97ae6fbcffacc28a4
Craig Livingstone <github@crzshadows.co.uk>
2024-12-29 18:20:47 +0000

    Made the route 103 cut tree required to get to the berry a one and done cut tree

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1270,7 +1270,7 @@
 #define FLAG_SCOTT_RECIEVED_CARBOS                                  0x4CA
 #define FLAG_BEAT_ARCHIE_MARINE_CAVE                                0x4CB
 #define FLAG_BEAT_MAXIE_TERRA_CAVE                                  0x4CC
-#define FLAG_UNUSED_0x4CD                                           0x4CD // Unused Flag
+#define FLAG_HIDE_ROUTE_103_TREE_1                                  0x4CD
 #define FLAG_UNUSED_0x4CE                                           0x4CE // Unused Flag
 #define FLAG_UNUSED_0x4CF                                           0x4CF // Unused Flag
 #define FLAG_UNUSED_0x4D0                                           0x4D0 // Unused Flag
